eba45677bf1afa80935b144a3c47178d753b2cea
AnwardeenM <kalilanwar597@gmail.com>
2022-11-05 22:58:32 +0530

    Created:Basic structure of the App

---
+++ employees/src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import EmployeeTable from '../Components/EmployeeTable'
+
+const HomePage = () => {
+  return (
+    <div>HomePage
+
+        <div>This is the Home Page with Add functionality</div>
+        <EmployeeTable/>
+    </div>
+  )
+}
+
+export default HomePage
